481b32dd80f183dd3d69deb6af6cc148c3c8523e
Mairaj <modmairaj30@gmail.com>
2023-12-21 12:55:23 +0530

    jenking

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String a[]){
         System.out.println("HEIllo Mairaj");
+        System.out.println("Wellcome Mairaj-----");
         //System.out.println(new Date().toString());
     }
 }
